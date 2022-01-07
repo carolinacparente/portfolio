a6aaa477dec136a015ca91ca4f75cf7e05aa0fb8
carolinacparente <94937217+carolinacparente@users.noreply.github.com>
2022-01-07 16:05:56 +0000

    Add files via upload
    portfolio-2022-janeiro-dw-mdm

---
+++ portfolio_2021_carolinaparente/js/about.js
@@ -0,0 +1,46 @@
+const about = document.querySelector(".about");
+const video = about.querySelector("video");
+const text = about.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: about,
+  triggerHook: 0
+})
+
+  .setPin(about)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 2500,
+  triggerElement: about,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1500;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  console.log(scrollpos, delay);
+
+  video.currentTime = delay;
+}, 33.3);
